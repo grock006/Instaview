c13f68241276ba7b5a59e4e68e4000398b28a01d
Greg Rock <grock006@gmail.com>
2015-01-25 14:43:48 -0800

    added background images to signup and login

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,6 @@ class FavoritesController < ApplicationController
 	end
 
 	def create
-		#@favorite = Favorite.new(name: params[:name], main_url: params[:main_url], profile_url: params[:profile_url])
 		@favorite = Favorite.new(params.require(:favorite).permit(:name, :main_url, :profile_url, :text, :icon, :username, :latitude, :longitude))
 		if @favorite.save
 			redirect_to favorites_path
