c8e214239865f6e9ab464344aee020cd8de4c3a6
Greg Rock <grock006@gmail.com>
2015-03-23 21:57:55 -0700

    updated images

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,6 @@ class User
 
   # has_secure_password 
   # validates :username, uniqueness: true
-
   # validates :first_name, presence: true
   # validates :last_name, presence: true
   # validates :email, presence: true, uniqueness: true
